0edb356c5cf9688dc8fcee177223bd888010f727
Islam <islamka.1994@mail.ru>
2024-02-05 03:32:48 +0300

    fix create_tables.sql and main.py

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,26 @@
 -- SQL-команды для создания таблиц
+create table employees
+(
+	employee_id int primary key,
+	first_name varchar(100),
+	last_name varchar(100),
+	title varchar(100),
+	birth_date varchar(100),
+	notes text
+);
+
+create table customers
+(
+	customer_id varchar(100) primary key,
+	company_name varchar(100),
+	contact_name varchar(100)
+);
+
+create table orders
+(
+	order_id int primary key,
+	customer_id varchar(100) references customers(customer_id),
+	employee_id int references employees(employee_id),
+	order_date varchar(100),
+	ship_city varchar(100)
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,18 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import pandas as pd
+import psycopg2
+
+employees = pd.read_csv('north_data/employees_data.csv').values.tolist()
+customers = pd.read_csv('north_data/customers_data.csv').values.tolist()
+orders = pd.read_csv('north_data/orders_data.csv').values.tolist()
+conn = psycopg2.connect(host='localhost', database='north', user='postgres', password='1405')
+try:
+    with conn:
+        with conn.cursor() as cur1:
+            cur1.executemany('insert into employees values(%s, %s, %s, %s, %s, %s)', employees)
+        with conn.cursor() as cur2:
+            cur2.executemany('insert into customers values(%s, %s, %s)', customers)
+        with conn.cursor() as cur3:
+            cur3.executemany('insert into orders values(%s, %s, %s, %s, %s)', orders)
+finally:
+    conn.close()
