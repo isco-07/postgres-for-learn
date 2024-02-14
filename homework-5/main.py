21988ea5048611bc6e4210e28c8b8a12b67de262
Islam <islamka.1994@mail.ru>
2024-02-14 07:09:07 +0300

    fix homework-5/main.py file

--- homework-5/main.py
+++ homework-5/main.py
@@ -31,9 +31,9 @@ def main():
                 print("Данные в suppliers успешно добавлены")
 
                 add_foreign_keys(cur, json_file)
-                print(f"FOREIGN KEY успешно добавлены")
+                print("FOREIGN KEY успешно добавлены")
 
-    except(Exception, psycopg2.DatabaseError) as error:
+    except (Exception, psycopg2.DatabaseError) as error:
         print(error)
     finally:
         if conn is not None:
@@ -42,26 +42,63 @@ def main():
 
 def create_database(params, db_name) -> None:
     """Создает новую базу данных."""
-    pass
+    try:
+        conn = psycopg2.connect(**params)
+        conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
+        with conn.cursor() as cur:
+            cur.execute(f"CREATE DATABASE {db_name}")
+    except psycopg2.Error as e:
+        print(f"Error creating database: {e}")
+    return
+
 
 def execute_sql_script(cur, script_file) -> None:
     """Выполняет скрипт из файла для заполнения БД данными."""
-
+    try:
+        with open(script_file, 'r', encoding='utf-8') as script:
+            cur.execute(script.read())
+    except FileNotFoundError as e:
+        print(f"Error opening script file: {e}")
+    except psycopg2.Error as e:
+        print(f"Error executing SQL script: {e}")
 
 
 def create_suppliers_table(cur) -> None:
     """Создает таблицу suppliers."""
-    pass
+    try:
+        cur.execute("""
+            CREATE TABLE IF NOT EXISTS suppliers 
+            (
+                supplier_id SERIAL PRIMARY KEY
+            )
+        """)
+    except psycopg2.Error as e:
+        print(f"Error creating suppliers table: {e}")
 
 
 def get_suppliers_data(json_file: str) -> list[dict]:
     """Извлекает данные о поставщиках из JSON-файла и возвращает список словарей с соответствующей информацией."""
-    pass
+    try:
+        with open(json_file, 'r', encoding='utf-8') as file:
+            suppliers_data = json.load(file)
+        return suppliers_data
+    except FileNotFoundError as e:
+        print(f"Error opening JSON file: {e}")
+        return []
 
 
 def insert_suppliers_data(cur, suppliers: list[dict]) -> None:
     """Добавляет данные из suppliers в таблицу suppliers."""
-    pass
+    try:
+        for i in suppliers[0].keys():
+            cur.execute(f"alter table suppliers add column {i} varchar")
+        for supplier in suppliers:
+            columns_str = ', '.join(supplier.keys())
+            values_str = ', '.join([f"%({column})s" for column in supplier.keys()])
+            sql_statement = f"INSERT INTO suppliers ({columns_str}) VALUES ({values_str});"
+            cur.execute(sql_statement, supplier)
+    except psycopg2.Error as e:
+        print(f"Error inserting data into suppliers table: {e}")
 
 
 def add_foreign_keys(cur, json_file) -> None:
